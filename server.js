fc278fb8c53577329bd2c2dc6670a555ee5db77c
Jack <Jack@example.com>
2025-08-08 10:55:02 +0800

    add query message

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
-const bcrypt      = require('bcrypt');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
+const bcrypt = require("bcrypt");
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
@@ -24,14 +27,13 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
-let hash= bcrypt.hashSync (myPlaintextPassword, saltRounds)
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 
-let result = bcrypt.compareSync (myPlaintextPassword, hash)
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 //END_SYNC
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log("Listening on port:", PORT);
 });
